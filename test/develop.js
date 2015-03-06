b60ac379ec62e0d9bb1acddb65ec8644af3e3107
Pascal Duez <public@pascalduez.me>
2015-03-06 12:57:58 +0100

    Add unit tests

---
+++ test/develop.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var postcss = require('postcss');
+var plugin = require('../');
+
+function read(name) {
+  return fs.readFileSync(path.join(__dirname, 'fixture', name), 'utf8');
+}
+
+var opts = {
+  basePath: 'test/fixture',
+  maps: [ 'dummy.yml', 'fonts.yml', 'breakpoints.yml' ]
+};
+
+['value', 'block', 'atrule'].forEach(function (test) {
+  var input = read(test + '/input.css');
+  var css = postcss(plugin(opts)).process(input).css;
+  console.log(css);
+})
--- test/map.test.js
+++ test/map.test.js
@@ -2,21 +2,45 @@
 
 var fs = require('fs');
 var path = require('path');
+var test = require('tape');
 var postcss = require('postcss');
 var plugin = require('../');
 
 function read(name) {
-  var p = path.join(__dirname, 'fixture', name);
-  return fs.readFileSync(p, 'utf8');
+  return fs.readFileSync(path.join(__dirname, 'fixture', name), 'utf8');
 }
 
 var opts = {
   basePath: 'test/fixture',
-  maps: [ 'fonts.yml', 'breakpoints.yml' ]
+  maps: [ 'dummy.yml', 'fonts.yml', 'breakpoints.yml' ]
 };
 
-var input = read('input.css');
+test('value', function (assert) {
+  assert.plan(1);
 
-var css = postcss(plugin(opts)).process(input).css;
+  var input = read('value/input.css');
+  var expected = read('value/expected.css');
+  var css = postcss(plugin(opts)).process(input).css;
 
-console.log(css);
+  assert.equal(css, expected);
+});
+
+test('block', function (assert) {
+  assert.plan(1);
+
+  var input = read('block/input.css');
+  var expected = read('block/expected.css');
+  var css = postcss(plugin(opts)).process(input).css;
+
+  assert.equal(css, expected);
+});
+
+test('atrule', function (assert) {
+  assert.plan(1);
+
+  var input = read('atrule/input.css');
+  var expected = read('atrule/expected.css');
+  var css = postcss(plugin(opts)).process(input).css;
+
+  assert.equal(css, expected);
+});
